515e22bdf4757fdc031a66b62657928825714131
Psaruklily <psarukli96@gmail.com>
2021-03-07 11:38:51 +0300

    Creater slider

--- js/script.js
+++ js/script.js
@@ -3,3 +3,18 @@ $('li a').click(function(e) {
     $('a').removeClass('active');
     $(this).addClass('active');
 });
+
+let offset = 0;
+const sliderLine = document.querySelector('.slider-line');
+
+document.querySelector('.fa-chevron-right').addEventListener('click', () => {
+    offset += 786;
+    if (offset > 2358) offset = 0;
+    sliderLine.style.left = -offset + 'px';
+});
+
+document.querySelector('.fa-chevron-left').addEventListener('click', () => {
+    offset -= 786;
+    if (offset < 0) offset = 2358;
+    sliderLine.style.left = -offset + 'px';
+});
