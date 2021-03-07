9b13c17b8bfc490223d93088a1959b04263fc1f6
Psaruklily <psarukli96@gmail.com>
2021-03-07 18:09:39 +0300

    Added thumbnails of images(rooms)

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ $('li a').click(function(e) {
     $('a').removeClass('active');
     $(this).addClass('active');
 });
-
+// Slider
 let offset = 0;
 const sliderLine = document.querySelector('.slider-line');
 
@@ -18,3 +18,21 @@ document.querySelector('.fa-chevron-left').addEventListener('click', () => {
     if (offset < 0) offset = 2358;
     sliderLine.style.left = -offset + 'px';
 });
+
+
+let smallRoom = document.querySelectorAll('.thumbnail');
+let slider = document.querySelector('.slider');
+let divActive = document.querySelector('.div-active');
+let imageActive = divActive.querySelector('img');
+
+for (let i = 0; i < smallRoom.length; i++) {
+    smallRoom[i].addEventListener('mouseover', () => {
+        slider.classList.add('hide');
+        divActive.classList.remove('hide');
+        imageActive.src = smallRoom[i].src;
+    });
+    smallRoom[i].addEventListener('mouseout', () => {
+        slider.classList.remove('hide');
+        divActive.classList.add('hide');
+    });
+}
